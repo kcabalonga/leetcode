d69cded40453c7708c89c8a9088b29d2baa5491e
Kirsten Reign Cabalonga <135193265+kcabalonga@users.noreply.github.com>
2025-05-20 18:18:02 -0700

    File updated at 5/20/2025, 6:18:02 PM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,13 +1,7 @@
-# Last updated: 5/20/2025, 6:12:15 PM
+# Last updated: 5/20/2025, 6:18:02 PM
 class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        i = 0
-        merged = ""
-        while i < max(len(word1), len(word2)):
-            if i < len(word1):
-                merged += word1[i]
-            if i < len(word2):
-                merged += word2[i]
-            i += 1
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 == str2 + str1:
+            return str1[:gcd(len(str1), len(str2))]
 
-        return merged
+        return ""
