cb14eeef900a97d499d46cd252c1945d631ae0a9
Kirsten Reign Cabalonga <135193265+kcabalonga@users.noreply.github.com>
2025-05-23 01:48:49 -0700

    File updated at 5/23/2025, 1:48:49 AM

--- 345-ReverseVowelsofaString/345-ReverseVowelsofaString.py
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.py
@@ -1,16 +1,15 @@
-# Last updated: 5/23/2025, 1:48:35 AM
-class Solution:
-    def reverseVowels(self, s: str) -> str:
-        vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
-        holder = [char for char in s if char in vowels]
-
-        reversed = ""
-
-        for char in s:
-            if char in vowels:
-                reversed += holder.pop()
-            else:
-                reversed += char
-
-        return reversed
-
+# Last updated: 5/23/2025, 1:48:49 AM
+class Solution(object):
+    def reverseVowels(self, s):
+        vowels = set("aeiouAEIOU")
+        s = list(s)
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and s[i] not in vowels:
+                i += 1
+            while i < j and s[j] not in vowels:
+                j -= 1
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        return "".join(s)
