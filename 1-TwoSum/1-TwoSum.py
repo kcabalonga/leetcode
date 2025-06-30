a769325633a8f2e92c0a26a4cdd51955e74e02ef
Kirsten Reign Cabalonga <135193265+kcabalonga@users.noreply.github.com>
2025-06-29 21:32:58 -0700

    File updated at 6/29/2025, 9:32:58 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,10 +1,9 @@
-# Last updated: 6/19/2025, 10:30:48 PM
+# Last updated: 6/29/2025, 9:32:58 PM
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash = {}
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in hash:
-                return [i, hash[complement]]
+                return [hash[complement], i]
             hash[nums[i]] = i
-        return []
